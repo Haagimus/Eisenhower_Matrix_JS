a92604e73c437403332de6baff385448e8f3f6e8
Haagimus <Haagimus@gmail.com>
2017-07-26 13:13:08 -0500

    Add files via upload
    added reset function and optimized the add task function

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ function setup() {
 
 function update() {
   var task = document.getElementById("task").value;
-  console.log(task);
   if (!task) {
     document.getElementById("submit").style.display = "none";
   } else {
@@ -20,17 +19,26 @@ function addTask() {
   var radioI = document.getElementsByName("importance");
   var radioU = document.getElementsByName("urgency");
 
-  for (var i = 0; i < radioI.length; i++) {
+  for (var i = 0; i < 2; i++) {
     if (radioI[i].checked) {
-      radioI = document.getElementById(radioI[i]).value;
+      // if (i = 0) {i = 1;} else {i = 2;};
+      radioI = document.getElementById("radio" + (i + 1)).value;
       break;
     }
   }
-  for (var i = 0; i < radioU.length; i++) {
+  for (var i = 0; i < 2; i++) {
     if (radioU[i].checked) {
-      radioU = document.getElementById(radioU[i]).value;
+      // if (i = 0) {i = 3;} else {i = 4;};
+      radioU = document.getElementById("radio" + (i + 3)).value;
       break;
     }
   }
-  console.log(summary, importance, urgency);
+  console.log(task, radioI, radioU);
+  reset();
+}
+
+function reset() {
+  document.getElementById("task").value = "";
+  document.getElementById("radio1").checked = true;
+  document.getElementById("radio3").checked = true;
 }
