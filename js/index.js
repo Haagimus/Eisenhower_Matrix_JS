54ff796b6e12379f0443c5c1d17a345e4b5b7231
Haagimus <Haagimus@gmail.com>
2017-07-26 18:29:42 -0500

    general update

--- js/index.js
+++ js/index.js
@@ -1,38 +1,41 @@
 function setup() {
-  document.getElementById("submit").style.display = "none";
   document.getElementById("task").value = "";
   document.getElementById("radio1").checked = true;
   document.getElementById("radio3").checked = true;
-}
-
-function update() {
-  var task = document.getElementById("task").value;
-  if (!task) {
-    document.getElementById("submit").style.display = "none";
-  } else {
-    document.getElementById("submit").style.display = "inline-block";
-  }
 }
 
 function addTask() {
   var task = document.getElementById("task").value;
   var radioI = document.getElementsByName("importance");
   var radioU = document.getElementsByName("urgency");
+  var grid;
 
   for (var i = 0; i < 2; i++) {
     if (radioI[i].checked) {
-      // if (i = 0) {i = 1;} else {i = 2;};
       radioI = document.getElementById("radio" + (i + 1)).value;
       break;
     }
   }
   for (var i = 0; i < 2; i++) {
     if (radioU[i].checked) {
-      // if (i = 0) {i = 3;} else {i = 4;};
       radioU = document.getElementById("radio" + (i + 3)).value;
       break;
     }
   }
+
+  switch (true) {
+    case radioI == "Important" && radioU == "Urgent":
+    grid = "iu";
+    console.log(grid);
+      break;
+    // default:
+
+  }
+
+  // var entry = document.createElement("li");
+  // entry.appendChild(document.createTextNode(task));
+
+
   reset();
 }
 
