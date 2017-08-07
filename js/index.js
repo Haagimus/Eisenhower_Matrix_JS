69b2f3aecffa2be18e5e6d328f2228b0ff1068ef
Haagimus <Haagimus@gmail.com>
2017-08-07 15:47:02 -0500

    Add files via upload
    added strike out task completion behavior

--- js/index.js
+++ js/index.js
@@ -32,12 +32,14 @@ function addTask() {
 
   if (!task) {
     alert("A task summary is required to \r\nadd a new task to the matrix.");
+    return;
   } else {
     grid = checkGrid(radioI, radioU);
   }
 
   var entry = document.createElement("h5");
   entry.innerHTML = task;
+  entry.className = "no-select";
   document.getElementById(grid).appendChild(entry);
   //console.log(grid, entry);
   resetForm();
@@ -56,8 +58,38 @@ function checkGrid(i, u) {
   }
 }
 
+function strikeOut(e) {
+  // e.target is the clicked element
+  // if the item clicked was an h5 element
+  if(e && e.localName == "h5") {
+
+    var isStruck = (e.style.textDecorationLineThrough);
+
+    if (isStruck) {
+      e.style.textDecorationLineThrough = false;
+      e.style.color = "black";
+    } else {
+      e.style.textDecorationLineThrough = true;
+      e.style.color = "grey";
+    }
+  }
+}
+
 function resetForm() {
   document.getElementById("task").value = "";
   document.getElementById("importance").value = "Important";
   document.getElementById("urgency").value = "Urgent";
 }
+
+document.getElementById("iu").addEventListener("dblclick", function(e) {
+  strikeOut(e.target);
+});
+document.getElementById("in").addEventListener("dblclick", function(e) {
+  strikeOut(e.target);
+});
+document.getElementById("nu").addEventListener("dblclick", function(e) {
+  strikeOut(e.target);
+});
+document.getElementById("nn").addEventListener("dblclick", function(e) {
+  strikeOut(e.target);
+});
